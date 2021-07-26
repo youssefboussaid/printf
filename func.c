238a4fa3b6622ea6bc1b12df49626068b8105b2a
youssefboussaid <85492375+youssefboussaid@users.noreply.github.com>
2021-07-26 10:59:34 -1100

    func.c

--- func.c
+++ func.c
@@ -30,20 +30,6 @@ int print_string(va_list ap)
 	}
 	return (i);
 }
-/**
- * print_int - function that prints an integer
- * @n: integer to be printed by function.
- */
-void print_int(int n)
-{
-	unsigned int m = n;
-
-	if (m / 10 != 0)
-	{
-		print_int(m / 10);
-	}
-	_putchar(m % 10 + '0');
-}
 
 /**
  *print_percent - print percent
@@ -56,4 +42,39 @@ int print_percent(va_list arg)
 
 	_putchar('%');
 	return (1);
+}
+
+/**
+ * _printint - function that prints an integer with helper function.
+ * @ap: The string to print, argument pointer
+ *
+ * Return: counter
+ *
+ */
+int print_int(va_list arg)
+{
+	int n = va_arg(arg, int);
+	int count = 0, rest = 1;
+	unsigned int x;
+	
+	x = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+		count++;
+	}
+
+	while ((x / rest) > 9)
+	{
+		rest = rest * 10;
+	}
+	while (rest > 0)
+	{
+		_putchar(x / rest + '0');
+		x = (x % rest);
+		rest = rest / 10;
+		count++;
+	}
+	return (count);
 }
