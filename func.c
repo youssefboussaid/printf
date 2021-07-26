b98852d05141d55ad8f9334c048338bd080b5b61
youssefboussaid <85492375+youssefboussaid@users.noreply.github.com>
2021-07-26 10:38:53 -1100

    func.c

--- func.c
+++ func.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 /**
- * _printchar - prints a character
+ * print_char - prints a character
  * @ap: The character to print, argument pointer
  *
  * Return: 1
  */
-int _printchar(va_list ap)
+int print_char(va_list ap)
 {
 	_putchar(va_arg(ap, int));
 	return (1);
 }
 /**
- * _printstring - prints a string
+ * print_string - prints a string
  * @ap: The string to print, argument pointer
  *
  * Return: i
  */
-int _printstring(va_list ap)
+int print_string(va_list ap)
 {
 	char *s = va_arg(ap, char *);
 	int i = 0;
@@ -31,57 +31,29 @@ int _printstring(va_list ap)
 	return (i);
 }
 /**
- * _printnum - function that prints an integer
+ * print_int - function that prints an integer
  * @n: integer to be printed by function.
  */
-void _printnum(int n)
+void print_int(int n)
 {
 	unsigned int m = n;
 
 	if (m / 10 != 0)
 	{
-		_printnum(m / 10);
+		print_int(m / 10);
 	}
 	_putchar(m % 10 + '0');
 }
-/**
- * _getcount - function that counts the length of an integer.
- * @n: integer to be counted.
- * Return: lenght of integer.
- */
-int _getcount(int n)
-{
-	int counter = 0;
-	unsigned int m = n;
-
-	while (m > 0)
-	{
-		m = m / 10;
-		counter++;
-	}
-	return (counter);
-}
 
 /**
- * _printint - function that prints an integer with helper function.
- * @ap: The string to print, argument pointer
- *
- * Return: counter
+ *print_percent - print percent
+ *@arg: argument of type of va_list
+ * Return: integer
  */
-int _printint(va_list ap)
+int print_percent(va_list arg)
 {
-	int counter = 0;
-	int n = va_arg(ap, int);
+	(void)arg;
 
-	if (n < 0)
-	{
-		n = -(n);
-		_putchar('-');
-		counter++;
-	}
-	if (n == 0)
-		counter++;
-	counter += _getcount(n);
-	_printnum(n);
-	return (counter);
+	_putchar('%');
+	return (1);
 }
