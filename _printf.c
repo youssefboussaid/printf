da8a99797a50f834d544d0b212ce151a55aa71d3
youssefboussaid <youssbsd@gmail.com>
2021-07-26 14:02:13 -0700

    prinf

--- _printf.c
+++ _printf.c
@@ -1,47 +1,41 @@
 #include "holberton.h"
-#include <stdarg.h>
 /**
- * _printf - produces output according to format
- * @format: format string containing zero or more directives
- *
- * Return: the number of characters printed, excluding null byte
+ * _printf - print all (printf reecreation)
+ * @format: format specifier
+ * Return: count
  */
 int _printf(const char *format, ...)
 {
-	data_t types[] = {
-		{"c", _printchar}, {"s", _printstring}, {"i", _printint},
-		{"R", _rot13}, {"d", _printint}, {"r", _printrev}};
-	int i, j, counter = 0;
-	va_list ap;
+	va_list arg;
+	int i = 0, count = 0, fun = 0;
 
-	va_start(ap, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
+	va_start(arg, format);
+	while (*(format + i) && format)
 	{
-		if (format[i] != '%')
-			counter += _putchar(format[i]);
-		else if (format[i + 1] == '%')
+		if (*(format + i) != '%')
 		{
-			counter += _putchar('%');
-			i++;
+			_putchar (*(format + i));
+			count++;
 		}
-		else if (format[i + 1] != 'c' && format[i + 1] != 's'
-			 && format[i + 1] != 'i' && format[i + 1] != 'd'
-			 && format[i + 1] != 'R' && format[i + 1] != 'r')
+		if (*(format + i) == '%')
 		{
-			counter += _putchar('%');
-			counter += _putchar(format[i + 1]);
-			i++;
+			fun = get_func(*(format + (i + 1)), arg);
+			if (fun != 0)
+			{
+				count = count + fun;
+				i = i + 2;
+				continue;
+			}
+			if (*(format + (i + 1)) == '\0')
+			{
+				_putchar(*(format + i));
+				count++;
+			}
 		}
-		else
-		{
-			for (j = 0; j < 6; j++)
-				if (format[i + 1] == *types[j].type)
-					counter += types[j].func(ap);
 			i++;
-		}
 	}
-	va_end(ap);
-	return (counter);
+	va_end(arg);
+	return (count);
 }
