f790f258a4cb552f3dc208778140cc56a17c8add
youssefboussaid <85492375+youssefboussaid@users.noreply.github.com>
2021-07-26 11:04:11 -1100

    main.c

--- main.c
+++ main.c
@@ -11,8 +11,7 @@ int main(void)
 {
     int len;
     int len2;
-    unsigned int ui;
-    void *addr;
+   
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
