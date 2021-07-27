cfeb3a0f6cd73ff5e83a0a528a6c3e7d9ae79092
youssefboussaid <youssbsd@gmail.com>
2021-07-27 03:05:55 -0700

    main.c

--- main.c
+++ main.c
@@ -9,15 +9,14 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-   
+	int len;
+	int len2;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    return (0);
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	return (0);
 }
