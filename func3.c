fa2be7a7ac91b35e8a4067f7145cd982d62d6b5c
youssefboussaid <85492375+youssefboussaid@users.noreply.github.com>
2021-07-27 01:12:25 -1100

    func3.c

---
+++ func3.c
@@ -0,0 +1,21 @@
+/**
+ * _printrev - prints a string, in reverse
+ * @ap: argument pointer to take arguments from
+ *
+ * Return: the counter of the string
+ */
+int _printrev(va_list ap)
+{
+
+	char *s = va_arg(ap, char*);
+	int c;
+	int i = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (s[i] != '\0')
+		i++;
+	for (c = i - 1; c >= 0; c--)
+		_putchar(s[c]);
+	return (i);
+}
