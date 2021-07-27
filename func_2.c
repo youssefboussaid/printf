e25f06eeac1b7ec6040ba9472454bea3e0546a94
youssefboussaid <85492375+youssefboussaid@users.noreply.github.com>
2021-07-27 01:06:50 -1100

    func_2.c

--- func_2.c
+++ func_2.c
@@ -132,3 +132,39 @@ int print_heX(va_list X)
 	}
 	return (count);
 }
+
+/**
+ * _rot13 - function that converts a string to rot13
+ * @ap: argument pointer to take arguments from.
+ * Return: the counter of the string.
+ */
+int _rot13(va_list ap)
+{
+	int i, j, counter = 0;
+	int f = 0;
+	char *s = va_arg(ap, char*);
+	char a[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char b[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
+	{
+		f = 0;
+		for (j = 0; a[j] && !f; j++)
+		{
+			if (s[i] == a[j])
+			{
+				_putchar(b[j]);
+				counter++;
+				f = 1;
+			}
+		}
+		if (!f)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
