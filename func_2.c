369d664e0cabefd46188b68e93e66e958b04437c
khouzi <khouzem.boussaid@gmail.com>
2021-07-27 04:33:15 -0700

    func_2.c

--- func_2.c
+++ func_2.c
@@ -99,3 +99,36 @@ int print_hex(va_list x)
 	}
 	return (count);
 }
+/**
+ * print_heX - prints input integer to an hexadecimal in uppercase
+ * @X: the list of arguments
+ * Return: number of printed characters
+ */
+int print_heX(va_list X)
+{
+	char hex[20];
+	unsigned int count = 0, n = 0;
+	int i = 0;
+
+	n = va_arg(X, unsigned int);
+	if (n == 0)
+	{
+		_putchar(0 + '0');
+		count++;
+	}
+	while (n > 0)
+	{
+		if (n % 16 >= 10 && n % 16 <= 15)
+			hex[i] = 55 + (n % 16);
+		else
+			hex[i] = 48 + (n % 16);
+		n = n / 16;
+		i++;
+	}
+	for (--i; i >= 0; i--)
+	{
+		_putchar(hex[i]);
+		count++;
+	}
+	return (count);
+}
