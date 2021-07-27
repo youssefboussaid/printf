1f50f4225f0206c14732ce7327e17ebc389ed4a4
youssefboussaid <youssbsd@gmail.com>
2021-07-27 03:20:50 -0700

    main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int len;
 	int len2;
-		
+
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	_printf("Length:[%d, %i]\n", len, len);
