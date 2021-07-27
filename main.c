48d08a3040cb034b136fd08495c914467b1287a3
khouzi <khouzem.boussaid@gmail.com>
2021-07-27 03:32:59 -0700

    main.c

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ int main(void)
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
